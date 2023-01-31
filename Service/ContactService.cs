21d9294c5a8bb11d32fad39d0a37480226ca1d72
rahmelldili <rameldili1@gmail.com>
2023-01-31 17:19:56 +0100

    Update

---
+++ Service/ContactService.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Microsoft.AspNetCore.Mvc.Razor.RuntimeCompilation;
+using System.Net.Mail;
+
+namespace Portfolio.Service
+{
+    public class ContactService
+    {
+        public static void SendMessage(string name, string email, string message)
+        {
+            // Create a new email message
+            var mailMessage = new MailMessage();
+            mailMessage.From = new MailAddress(email, name);
+            mailMessage.To.Add("Rameldili1@gmail.com");
+            mailMessage.Subject = "Contact Form Submission";
+            mailMessage.Body = message;
+
+            // Send the email
+            using (var smtp = new SmtpClient())
+            {
+
+                smtp.Send(mailMessage);
+            }
+
+        }
+    }
+}
+
---
+++ wwwroot/js/site.js
@@ -0,0 +1,38 @@
+﻿document.getElementById("contact-form").addEventListener("submit", function (event) {
+    event.preventDefault();
+    var name = document.getElementById("name").value;
+    var email = document.getElementById("email").value;
+    var message = document.getElementById("message").value;
+    if (!name || !email || !message) {
+        alert("Please fill out all fields!");
+        return;
+    }
+    if (!email.includes("@")) {
+        alert("Please enter a valid email address!");
+        return;
+    }
+
+    // Send the form data to the server
+    //...
+    document.getElementById("contact-form").addEventListener("submit", function (event) {
+        event.preventDefault();
+        var name = document.getElementById("name").value;
+        var email = document.getElementById("email").value;
+        var message = document.getElementById("message").value;
+        // Send a post request to the server
+        fetch("/submit-form", {
+            method: "POST",
+            body: JSON.stringify({ name: name, email: email, message: message }),
+            headers: { "Content-Type": "application/json" }
+        })
+            .then(response => response.json())
+            .then(data => {
+                if (data.success) {
+                    alert("Form submitted successfully!");
+                } else {
+                    alert("An error occurred: " + data);
+                }
+
+            }
+
+
